14f2667637281f4169b7f0562740668983a51539
Dmitry Vorobjev <vorobiov17@yandex.ru>
2018-01-30 01:25:08 +0400

    update data

--- merge_dataframes.py
+++ merge_dataframes.py
@@ -1,17 +1,38 @@
-import re
 import csv
-import glob
 import pandas as pd
-import numpy as np
 
-sortedData = csv.reader(open('result_eur.csv', 'r'))
-sortedData = sorted(sortedData)
-sortedDatac = csv.reader(open('EURUSD30.csv', 'r'))
-sortedDatac = sorted(sortedDatac)
+sortedDataEur = csv.reader(open('result_eur.csv', 'r'))
+sortedDataEur = sorted(sortedDataEur)
+sortedDataCurrency = csv.reader(open('EURUSD30.csv', 'r'))
+sortedDataCurrency = sorted(sortedDataCurrency)
+sortedDataUsd = csv.reader(open('result_usd.csv', 'r'))
+sortedDataUsd = sorted(sortedDataUsd)
+sortedDataAurum = csv.reader(open('XAUUSD30.csv', 'r'))
+sortedDataAurum = sorted(sortedDataAurum)
 
-df = pd.DataFrame(sortedData, columns=['date', 'usd_sent_mean', 'usd_subj_mean', 'usd_sent_std', 'usd_subj_std'])
-dfc = pd.DataFrame(sortedDatac, columns=['date', 'open', 'high', 'low', 'close', 'vol'])
+# dfEur = pd.DataFrame(sortedDataEur, columns=['date', 'eur_sent_mean', 'eur_subj_mean', 'eur_sent_std', 'eur_subj_std'])
+# dfUsd = pd.DataFrame(sortedDataUsd, columns=['date', 'usd_sent_mean', 'usd_subj_mean', 'usd_sent_std', 'usd_subj_std'])
+#
+# df = dfEur.set_index('date').join(dfUsd.set_index('date'))
+#
+# df.to_csv('merged.csv')
 
-df.set_index('date').join(dfc.set_index('date'))
+
+# sortedDataEurUsd = csv.reader(open('merged.csv', 'r'))
+# sortedDataEurUsd = sorted(sortedDataEurUsd)
+#
+# dfEurUsd = pd.DataFrame(sortedDataEurUsd, columns=['date', 'eur_sent_mean', 'eur_subj_mean', 'eur_sent_std', 'eur_subj_std', 'usd_sent_mean', 'usd_subj_mean', 'usd_sent_std', 'usd_subj_std', 'eurusd_open', 'eurusd_high', 'eurusd_low', 'eurusd_close', 'eurusd_vol'])
+# dfCurrency = pd.DataFrame(sortedDataCurrency, columns=['date', 'open', 'high', 'low', 'close', 'vol'])
+#
+# df = dfEurUsd.set_index('date').join(dfCurrency.set_index('date'))
+
+
+sortedDataEurUsdCurrency = csv.reader(open('merged.csv', 'r'))
+sortedDataEurUsdCurrency = sorted(sortedDataEurUsdCurrency)
+
+dfEurUsdCurency = pd.DataFrame(sortedDataEurUsdCurrency, columns=['date', 'eur_sent_mean', 'eur_subj_mean', 'eur_sent_std', 'eur_subj_std', 'usd_sent_mean', 'usd_subj_mean', 'usd_sent_std', 'usd_subj_std', 'eurusd_open', 'eurusd_high', 'eurusd_low', 'eurusd_close', 'eurusd_vol'])
+dfCurrencyAur = pd.DataFrame(sortedDataAurum, columns=['date', 'aur_open', 'aur_high', 'aur_low', 'aur_close', 'aur_vol'])
+
+df = dfEurUsdCurency.set_index('date').join(dfCurrencyAur.set_index('date'))
 
 df.to_csv('merged.csv')
